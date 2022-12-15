5dbb5ca434784bf5390112f92cf3a14333b3971d
Bea Tendero <beatriz.tendero.hernandez@gmail.com>
2022-12-15 12:11:11 +0100

    add super admin role to user by id user

--- auth-laravel/routes/api.php
+++ auth-laravel/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
